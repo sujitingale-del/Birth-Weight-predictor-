3a2a36948f2f566a75ef0a414a87bbe1feb989c3
sujitingale-del <sujitingale6191@gmail.com>
2026-01-22 11:55:18 +0530

    my second commit

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_prediction():
    baby_df = pd.DataFrame(baby_data_cleaned)
 
    #load the model
-   with open("model/model.pkl", 'rb') as obj:
+   with open("model.pkl", 'rb') as obj:
        model = pickle.load(obj)
        
    #make prediction
